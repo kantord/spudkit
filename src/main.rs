392b8f3b4fe0c7b5ef21b49f0deb46c3b7ea0590
Daniel Kantor <git@daniel-kantor.com>
2026-03-19 21:48:11 +0100

    add hello world server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3030").await.unwrap();
+    println!("Listening on http://localhost:3030");
+    axum::serve(listener, app).await.unwrap();
 }
